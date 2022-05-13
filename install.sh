b21a9e71cdc59f20af3aaf95dae33e9700d63a0a
isaac-gardner <58774715+isaac-gardner@users.noreply.github.com>
2022-05-13 21:41:01 +1000

    forgot docker compose

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt-get -y install apt-transport-https ca-certificates gnupg-agent software
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
 sudo apt-get update 
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io 
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose
 
 curl -o docker-compose.yml https://raw.githubusercontent.com/isaac-gardner/FernGully/main/docker-compose.yml
 
