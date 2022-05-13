dd659c3fde3707d433d3de1ceed90ff1785f174f
isaac-gardner <58774715+isaac-gardner@users.noreply.github.com>
2022-05-13 21:39:58 +1000

    up

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
 sudo apt-get update 
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io 
+
+curl -o docker-compose.yml https://raw.githubusercontent.com/isaac-gardner/FernGully/main/docker-compose.yml
+
+docker-compose up -d
